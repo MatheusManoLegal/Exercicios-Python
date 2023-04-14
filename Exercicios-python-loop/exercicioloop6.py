0541640c1ceaf4e33a23ad3abf605a1135e6cd0a
unknown <Matheus.SantosTSIS@gmail.com>
2023-04-14 14:25:41 -0300

    Adicionado exercicios que estavam faltando

---
+++ Exercicios-python-loop/exercicioloop6.py
@@ -0,0 +1,9 @@
+# Imprime os números de 1 a 20 um abaixo do outro
+print("Números de 1 a 20 um abaixo do outro:")
+for i in range(1, 21):
+    print(i)
+
+# Imprime os números de 1 a 20 um ao lado do outro
+print("\nNúmeros de 1 a 20 um ao lado do outro:")
+for i in range(1, 21):
+    print(i, end=" ")
