63bf8b4cd5966c61c09c1e8682d80f3dfcc1badf
Alzahem <124899897+MatheusManoLegal@users.noreply.github.com>
2023-04-14 11:13:59 -0300

    Delete Exercicios-python directory

--- Exercicios-python/exercicio2.py
+++
@@ -1,9 +0,0 @@
-vetor = []
-for i in range(10):
-    num = float(input(f"Digite o {i+1}º número real: "))
-    vetor.append(num)
-
-# Impressão do vetor na ordem inversa
-print("Vetor na ordem inversa:")
-for i in range(9, -1, -1):
-    print(vetor[i])
