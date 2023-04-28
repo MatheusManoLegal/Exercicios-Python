51d4b3a603fcca1b5c9bed43cb47e0819f8f43f1
Alzahem <124899897+MatheusManoLegal@users.noreply.github.com>
2023-04-28 10:28:26 -0300

    Delete Exercicios-python-loop directory

--- Exercicios-python-loop/exercicioloop1.py
+++
@@ -1,7 +0,0 @@
-while True:
-    nota = float(input("Por favor, informe uma nota entre zero e dez: "))
-    if nota >= 0 and nota <= 10:
-        print("Nota válida:", nota)
-        break
-    else:
-        print("Valor inválido. Por favor, informe uma nota entre zero e dez.")
--- Exercicios-python-loop/exercicioloop6.py
+++
@@ -1,9 +0,0 @@
-# Imprime os números de 1 a 20 um abaixo do outro
-print("Números de 1 a 20 um abaixo do outro:")
-for i in range(1, 21):
-    print(i)
-
-# Imprime os números de 1 a 20 um ao lado do outro
-print("\nNúmeros de 1 a 20 um ao lado do outro:")
-for i in range(1, 21):
-    print(i, end=" ")
